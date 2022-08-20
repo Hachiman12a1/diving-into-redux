f56ea925c138001a3f79e9d8d11f45775b2b013d
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-20 15:09:26 +0700

    239. Working with Multiple State Properties

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
 
   const dispatch = useDispatch();
 
@@ -18,12 +19,15 @@ const Counter = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button className={classes.button} onClick={incrementHandler}>
           Increment
